9a31a5707a6ba0eea1697c01497e2c81d5999e7f
SK TOUFIK ISLAM <randomstuff.toufik@gmail.com>
2026-05-16 11:44:11 +0530

    Delete Quiz directory
    Not needed

--- Quiz/backend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const auth = require("../middleware/auth");
-const role = require("../middleware/roles");
-const { getUsers, analytics } = require("../controllers/adminController");
-
-router.get("/users", auth, role("admin"), getUsers);
-router.get("/analytics", auth, role("admin"), analytics);
-
-module.exports = router;
